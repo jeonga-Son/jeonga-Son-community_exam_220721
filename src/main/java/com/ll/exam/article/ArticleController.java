d1de84216e105921e3dff2ceb51943b9fdace947
jeonga-Son <jjeong0106@gmail.com>
2022-07-25 15:38:26 +0900

    게시물 객체 3개를 노출

---
+++ src/main/java/com/ll/exam/article/ArticleController.java
@@ -0,0 +1,19 @@
+package com.ll.exam.article;
+
+import com.ll.exam.Rq;
+import com.ll.exam.article.dto.ArticleDto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArticleController {
+    public void showList(Rq rq) {
+        List<ArticleDto> articleDtos = new ArrayList<>();
+        articleDtos.add(new ArticleDto(1, "제목 3", "내용 3"));
+        articleDtos.add(new ArticleDto(1, "제목 2", "내용 2"));
+        articleDtos.add(new ArticleDto(1, "제목 1", "내용 1"));
+
+        rq.setAttr("articles", articleDtos);
+        rq.view("usr/article/list");
+    }
+}
