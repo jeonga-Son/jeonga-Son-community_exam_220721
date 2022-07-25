8175d5d9f9288f796f28fc4b9ffc4ed4d8873813
jeonga-Son <jjeong0106@gmail.com>
2022-07-25 15:26:56 +0900

    라우팅 기능 구현

--- src/main/java/com/ll/exam/DispatchServlet.java
+++ src/main/java/com/ll/exam/DispatchServlet.java
@@ -7,7 +7,9 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
+
 @WebServlet("/usr/*")
 public class DispatchServlet extends HttpServlet {
     @Override
